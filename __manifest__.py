49728e75d6a8b454e91d97e2f7613a2e0ea91734
Zarcam <zarcam.t@gmail.com>
2024-02-02 17:16:32 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Lista de tareas (Calender)",
+    'description': """Lista de tareas en formato calendario""",
+    'category': 'Project',
+    'version': 1.0,
+    'application': True,
+    'depends': ['project'],
+    'data': [
+
+    ],
+}
